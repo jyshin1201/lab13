9dd3947a2e973a1d1596551d3a5ab36777dd4641
JY <88931301+jyshin1201@users.noreply.github.com>
2024-06-10 12:43:55 +0900

    Update sale.h

--- git/sale.h
+++ git/sale.h
@@ -1,13 +1,33 @@
-//
-//  1.hpp
-//  git
-//
-//  Created by Jiyun Shin on 2024/06/10.
-//
+#pragma once
+#include <iostream>
 
-#ifndef __hpp
-#define __hpp
+class sale
+{
+    double price;
+public:
+    sale(double p = 0) {setPrice(p);}
+    double getPrice() const {return price;}
+    void setPrice(double n)
+    {
+        if (n>=0)
+            price = n;
+        else
+        {
+            std::cout << "Error in sale::setPrice(), cannot have a negative price!\n";
+            std::exit(1);
+        }
+    }
+    double savings(const sale& other) const
+    {
+        return bill() - other.bill();
+    }
+    virtual double bill() const
+    {
+        return price;
+    }
+};
 
-#include <stdio.h>
-
-#endif /* __hpp */
+bool operator<(const sale& first, const sale& second)
+{
+    return first.bill() < second.bill();
+}
